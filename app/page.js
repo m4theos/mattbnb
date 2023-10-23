b3ff94576120e113695007b5cfd23d094b145c5d
Matthaios Petrou <m.petrou-1@student.utwente.nl>
2023-10-23 15:05:42 +0200

    project reset --added build-in routing

---
+++ app/page.js
@@ -0,0 +1,11 @@
+export default function Home() {
+  return (
+    <div className="flex flex-col items-center justify-center 
+    min-h-screen py-2">
+      {/* Header */}
+      
+      {/* Banner */}
+    </div>
+
+  )
+}
